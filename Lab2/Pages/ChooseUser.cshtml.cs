3ea297670d2acd8f981f4edb466cc9eb137c3586
Ben Trang <121653356+BT-Compile@users.noreply.github.com>
2023-09-29 15:58:44 -0400

    Worked on chooseuser page

--- Lab2/Pages/ChooseUser.cshtml.cs
+++ Lab2/Pages/ChooseUser.cshtml.cs
@@ -1,12 +1,61 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab2.Pages
 {
     public class ChooseUserModel : PageModel
     {
+        [BindProperty]
+        public String Username { get; set; }
+
+        [BindProperty]
+        public String Password { get; set; }
+
+        [BindProperty]
+        public String Email { get; set; }
+
+        [BindProperty]
+        public String Phone { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Lab2.Pages.DB.DBClass.SecureLogin(Username, Password) > 0)
+            {
+                HttpContext.Session.SetString("username", Username);
+                ViewData["LoginMessage"] = "Login Successful!";
+
+            }
+            else
+            {
+                ViewData["LoginMessage"] = "Username and/or Password Incorrect";
+
+            }
+
+            Lab2.Pages.DB.DBClass.Lab2DBConnection.Close();
+
+            return Page();
+        }
+
+        // Handler for clearing text fields
+        public IActionResult OnPostClearHandler()
+        {
+            return Page();
+        }
+
+        // Handler for populating text fields
+        public IActionResult OnPostPopulateHandler()
+        {
+            ModelState.Clear(); // used to ignore validation
+            Username = "";
+            Password = "";
+            Email = "";
+            Phone = "";
+            return Page();
         }
     }
 }
--- Lab2/Pages/DB/DBClass.cs
+++ Lab2/Pages/DB/DBClass.cs
@@ -38,5 +38,28 @@ namespace Lab2.Pages.DB
             return tempReader;
         }
 
+        public static int SecureLogin(string Username, string Password)
+        {
+            string loginQuery =
+                "SELECT COUNT(*) FROM Credentials where Username = @Username and Password = @Password";
+
+            SqlCommand cmdLogin = new SqlCommand();
+            cmdLogin.Connection = Lab2DBConnection;
+            cmdLogin.Connection.ConnectionString = Lab2DBConnString;
+
+            cmdLogin.CommandText = loginQuery;
+            cmdLogin.Parameters.AddWithValue("@Username", Username);
+            cmdLogin.Parameters.AddWithValue("@Password", Password);
+
+            cmdLogin.Connection.Open();
+
+            // ExecuteScalar() returns back data type Object
+            // Use a typecast to convert this to an int.
+            // Method returns first column of first row.
+            int rowCount = (int)cmdLogin.ExecuteScalar();
+
+            return rowCount;
+        }
+
     }
 }
