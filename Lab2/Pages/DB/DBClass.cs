06378e77d1e1f7a86fc7c102feb9b21646eba853
Ben Trang <121653356+BT-Compile@users.noreply.github.com>
2023-09-29 15:22:10 -0400

    Created a bunch of pages for end user to go through

---
+++ Lab2/Pages/DB/DBClass.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+
+namespace Lab2.Pages.DB
+{
+    public class DBClass
+    {
+
+        // Connection object at the class level
+        public static SqlConnection Lab2DBConnection = new SqlConnection();
+
+        // Connection string
+        // ";TrustServerCertificate=True"
+        public static readonly String Lab2DBConnString = "Server=Localhost; Database=Lab2; Trusted_Connection=True";
+
+        public static SqlDataReader UserReader()
+        {
+            SqlCommand cmdProductRead = new SqlCommand();
+            cmdProductRead.Connection = Lab2DBConnection;
+            cmdProductRead.Connection.ConnectionString = Lab2DBConnString;
+            cmdProductRead.CommandText = "SELECT * FROM User";
+            cmdProductRead.Connection.Open();
+
+            SqlDataReader tempReader = cmdProductRead.ExecuteReader();
+
+            return tempReader;
+        }
+
+        public static SqlDataReader EventReader()
+        {
+            SqlCommand cmdProductRead = new SqlCommand();
+            cmdProductRead.Connection = Lab2DBConnection;
+            cmdProductRead.Connection.ConnectionString = Lab2DBConnString;
+            cmdProductRead.CommandText = "SELECT * FROM Event";
+            cmdProductRead.Connection.Open();
+
+            SqlDataReader tempReader = cmdProductRead.ExecuteReader();
+
+            return tempReader;
+        }
+
+    }
+}
---
+++ Lab2/Pages/DBLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab2.Pages
+{
+    public class DBLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
